74a2d363f1e1266667c8dd853e6ad2bb9285026e
yi-ge-dian <1085266008@qq.com>
2023-06-18 13:21:27 +0800

    add tsc config

---
+++ 09_configuration/src/index.ts
@@ -0,0 +1,14 @@
+interface Chicken {
+    breed: string;
+    eggsPerWeek: number;
+    name: string;
+    age: number;
+}
+
+const norma: Chicken = {
+    breed: "Silkie",
+    eggsPerWeek: 4,
+    name: "Norma",
+    age: 20
+}
+
